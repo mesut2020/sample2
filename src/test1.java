9a36a8a7775b65d791c102e9aed94da7138ca378
mesut2020 <medogan@gmail.com>
2020-11-24 20:33:43 +0100

    new method is added to add cart

--- src/test1.java
+++ src/test1.java
@@ -2,4 +2,9 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("Test 1");
     }
+
+    public static void newMethod(){
+        System.out.println("My new method is ready...");
+        System.out.println("Add cart is here...");
+    }
 }
